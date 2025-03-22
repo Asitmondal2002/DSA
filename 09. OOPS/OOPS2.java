7fecfd29023b40a0ff246f39a569fae712ff37f1
Asitmondal2002 <mondalasit0143@gmail.com>
2025-03-22 19:36:46 +0530

    getter and setter methods

---
+++ 09. OOPS/OOPS2.java
@@ -0,0 +1,24 @@
+//getter and setter methods
+public class OOPS2 {
+    public static void main(String args[]) {
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Aditya";
+        myAcc.setPassword("123456");
+        System.out.println(myAcc.getPassword());
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    // setter
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // getter
+    String getPassword() {
+        return password;
+    }
+}
